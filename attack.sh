fe72e92b5413a571f09a10c366b8e932ecb0f15b
yul091 <yul091@ucsd.edu>
2023-06-08 10:20:58 -0700

    requirements

--- attack.sh
+++ attack.sh
@@ -1,8 +1,8 @@
 ATT_METHOD=prompt # prompt, fd, hotflip, textbugger, uat
 MAX_PER=5
-MODEL_PATH=Microsoft/DialoGPT-medium # microsoft/DialoGPT-large, microsoft/DialoGPT-medium, microsoft/DialoGPT-small, facebook/blenderbot-400M-distill, facebook/blenderbot-1B-distill
+MODEL_PATH=microsoft/DialoGPT-medium # microsoft/DialoGPT-large, microsoft/DialoGPT-medium, microsoft/DialoGPT-small, facebook/blenderbot-400M-distill, facebook/blenderbot-1B-distill
 DATASET=blended_skill_talk # blended_skill_talk, conv_ai_2, empathetic_dialogues, AlekseyKorshuk/persona-chat
-FITNESS=adaptive # performance, length, combined, adaptive
+FITNESS=length # performance, length, combined, adaptive
 NUM_SAMPLES=30
 MAX_LENGTH=256
 SELECT_BEAMS=2
@@ -16,5 +16,5 @@ CUDA_VISIBLE_DEVICES=1 python -W ignore attack.py \
     --max_len $MAX_LENGTH \
     --select_beams $SELECT_BEAMS \
     --out_dir logging \
-    --fitness $FITNESS \
-    --use_combined_loss
+    --fitness $FITNESS
+    # --use_combined_loss
